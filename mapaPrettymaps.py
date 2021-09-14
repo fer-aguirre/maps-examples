acfcdd43cb8e2f17a7964de443c66090a5f50ad0
FXR <kay_op@protonmail.com>
2021-09-14 18:46:38 -0500

    Add examples

--- mapaPrettymaps.py
+++ mapaPrettymaps.py
@@ -1,98 +1,88 @@
 from prettymaps import *
 import matplotlib.font_manager as fm
 from matplotlib import pyplot as plt
-#import vsketch
 
-# ### square plot with rounded edges
-# # Style parameters
-# palette = ['#542362', '#FF5E5B']
-# background_c = '#F6CCEE'
-# dilate = 100
-#
-# # Setup figure
-# fig, ax = plt.subplots(figsize = (10, 10), constrained_layout = True)
-#
-# # Plot
-# layers = plot(
-#     (19.43274052957381, -99.133221555212), radius = 500,
-#     ax = ax,
-#     layers = {
-#         'perimeter': {'circle': False, 'dilate': dilate},
-#         'streets': {
-#             'width': {
-#                 'primary': 5,
-#                 'secondary': 4,
-#                 'tertiary': 3,
-#                 'residential': 2,
-#                 'footway': 1,
-#             },
-#             'circle': False,
-#             'dilate': dilate
-#         },
-#         'building': {
-#             'tags': {'building': True},
-#             'union': False,
-#             'circle': False,
-#             'dilate': dilate
-#         },
-#         'green': {
-#             'tags': {
-#                 'landuse': ['grass', 'village_green'],
-#                 'leisure': 'park'
-#             },
-#             'circle': False,
-#             'dilate': dilate
-#         },
-#     },
-#     drawing_kwargs = {
-#         'background': {'fc': '#F2F4CB', 'ec': '#dadbc1', 'hatch': 'ooo...', 'zorder': -1},
-#         'perimeter': {'fill': False, 'lw': 0, 'zorder': 0},
-#         'green': {'fc': '#8BB174', 'ec': '#2F3737', 'hatch_c': '#A7C497', 'hatch': 'ooo...', 'lw': 1, 'zorder': 1},
-#         'water': {'fc': '#a8e1e6', 'ec': '#2F3737', 'hatch_c': '#9bc3d4', 'hatch': 'ooo...', 'lw': 1, 'zorder': 3},
-#         'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 4},
-#         'building': {'palette': palette, 'ec': '#2F3737', 'lw': .5, 'zorder': 5},
-#     },
-#     osm_credit = {'x': .02, 'y': .01, 'color': '#2F3737'}
-# )
-#
-# # Set bounds
-# xmin, ymin, xmax, ymax = layers['perimeter'].bounds
-# dx, dy = xmax-xmin, ymax-ymin
-# ax.set_xlim(xmin-.06*dx, xmax+.06*dx)
-# ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
-#
-# # Draw left text
-# ax.text(
-#     xmin-.06*dx, ymin+.5*dy,
-#     'CDMX, México',
-#     color = '#2F3737',
-#     rotation = 90,
-#     fontname=' DejaVu Sans', fontsize=35
-#     #fontproperties = fm.FontProperties(fname = '../assets/Permanent_Marker/PermanentMarker-Regular.ttf', size = 35),
-# )
-# # Draw top text
-# ax.text(
-#     xmax-.35*dx, ymax+.02*dy,
-#     #"41° 23′ N, 2° 11′ E",
-#     color = '#2F3737',
-#     fontname=' DejaVu Sans', fontsize=20
-#     #fontproperties = fm.FontProperties(fname = '../assets/Permanent_Marker/PermanentMarker-Regular.ttf', size = 20),
-# )
-#
-# plt.savefig('prints/cdmx-square.png')
-# #"plt.savefig('prints/cdmx-square.svg')
+
+### square plot with rounded edges
+
+# Style parameters
+palette = ['#001c5a', '#ffab15']
+background_c = '#e5c6f5'
+dilate = 100
+
+# Setup figure
+fig, ax = plt.subplots(figsize = (10, 10), constrained_layout = True)
+
+# Plot
+layers = plot(
+    (19.43274052957381, -99.133221555212), radius = 500,
+    ax = ax,
+    layers = {
+        'perimeter': {'circle': False, 'dilate': dilate},
+        'streets': {
+            'width': {
+                'primary': 5,
+                'secondary': 4,
+                'tertiary': 3,
+                'residential': 2,
+                'footway': 1,
+            },
+            'circle': False,
+            'dilate': dilate
+        },
+        'building': {
+            'tags': {'building': True},
+            'union': False,
+            'circle': False,
+            'dilate': dilate
+        },
+        'green': {
+            'tags': {
+                'landuse': ['grass', 'village_green'],
+                'leisure': 'park'
+            },
+            'circle': False,
+            'dilate': dilate
+        },
+    },
+    drawing_kwargs = {
+        'background': {'fc': '#fdf9a0', 'ec': '#ffedac', 'hatch': 'ooo...', 'zorder': -1},
+        'perimeter': {'fill': False, 'lw': 0, 'zorder': 0},
+        'green': {'fc': '#8BB174', 'ec': '#2F3737', 'hatch_c': '#A7C497', 'hatch': 'ooo...', 'lw': 1, 'zorder': 1},
+        'water': {'fc': '#a8e1e6', 'ec': '#2F3737', 'hatch_c': '#9bc3d4', 'hatch': 'ooo...', 'lw': 1, 'zorder': 3},
+        'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 4},
+        'building': {'palette': palette, 'ec': '#005858', 'lw': .5, 'zorder': 5},
+    },
+    osm_credit = {'x': .02, 'y': .01, 'color': '#2F3737'}
+)
+
+# Set bounds
+xmin, ymin, xmax, ymax = layers['perimeter'].bounds
+dx, dy = xmax-xmin, ymax-ymin
+ax.set_xlim(xmin-.06*dx, xmax+.06*dx)
+ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
+
+# Draw top text
+ax.text(
+    xmax-.30*dx, ymax+.01*dy,
+    'CDMX, México',
+    color = '#2F3737',
+    fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40))
+
+plt.savefig('ejemplos/cdmx-square.png')
+
 
 ###############################################################################
 
 ### circle plot
+
+Setup figure
 fig, ax = plt.subplots(figsize = (12, 12), constrained_layout = True)
 
 layers = plot(
     'CDMX, México', radius = 1100,
 
     ax = ax,
-
-    #backup = layers,
 
     layers = {
             'perimeter': {},
@@ -143,11 +133,9 @@ ax.text(
     xmax-.25*dx, ymax-.02*dy,
     'CDMX, México',
     color = '#2F3737',
-    #fontfamily=' DejaVu Sans',
-    #fontsize=30
     fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40),
 ),
 
-plt.savefig('output/cdmx-circle2.png')
+plt.savefig('ejemplos/cdmx-circle.png')
 
 ###############################################################################
