6390b70e4d820ec6d7a328a8c99e80dd7c888c0b
FXR <kay_op@protonmail.com>
2021-09-15 11:33:49 -0500

    Update maps

--- mapaPrettymaps.py
+++ mapaPrettymaps.py
@@ -2,79 +2,6 @@ from prettymaps import *
 import matplotlib.font_manager as fm
 from matplotlib import pyplot as plt
 
-# SQUARE PLOT WITH ROUNDED EDGES
-# Style parameters
-palette = ['#001c5a', '#ffab15']
-background_c = '#e5c6f5'
-dilate = 100
-
-# Setup figure
-fig, ax = plt.subplots(figsize = (10, 10), constrained_layout = True)
-
-# Plot
-layers = plot(
-    (19.449885835076476, -99.15688024541792), radius = 500,
-    ax = ax,
-    layers = {
-        'perimeter': {'circle': False, 'dilate': dilate},
-        'streets': {
-            'width': {
-                'primary': 5,
-                'secondary': 4,
-                'tertiary': 3,
-                'residential': 2,
-                'footway': 1,
-            },
-            'circle': False,
-            'dilate': dilate
-        },
-        'building': {
-            'tags': {'building': True},
-            'union': False,
-            'circle': False,
-            'dilate': dilate
-        },
-        'green': {
-            'tags': {
-                'landuse': ['grass', 'village_green'],
-                'leisure': 'park'
-            },
-            'circle': False,
-            'dilate': dilate
-        },
-    },
-    drawing_kwargs = {
-        'background': {'fc': '#fdf9a0', 'ec': '#ffedac', 'hatch': 'ooo...', 'zorder': -1},
-        'perimeter': {'fill': False, 'lw': 0, 'zorder': 0},
-        'green': {'fc': '#8BB174', 'ec': '#2F3737', 'hatch_c': '#A7C497', 'hatch': 'ooo...', 'lw': 1, 'zorder': 1},
-        'water': {'fc': '#a8e1e6', 'ec': '#2F3737', 'hatch_c': '#9bc3d4', 'hatch': 'ooo...', 'lw': 1, 'zorder': 3},
-        'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 4},
-        'building': {'palette': palette, 'ec': '#005858', 'lw': .5, 'zorder': 5},
-    },
-    osm_credit = {'x': .02, 'y': .01, 'color': '#2F3737'}
-)
-
-# Set bounds
-xmin, ymin, xmax, ymax = layers['perimeter'].bounds
-dx, dy = xmax-xmin, ymax-ymin
-ax.set_xlim(xmin-.06*dx, xmax+.06*dx)
-ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
-
-# Draw right text
-ax.text(
-    #xmin-.05*dx, ymin-.06*dy,
-    xmin+1.02*dx, ymin+.26*dy,
-    'Kiosco Morisco, CDMX, México',
-    color = '#2F3737',
-    rotation = 270,
-    fontproperties = fm.FontProperties(fname = 'assets/fonts/HashedBrowns-WyJgn.ttf', size = 40)
-)
-
-plt.savefig('ejemplos/kiosco-morisco.png')
-
-
-###############################################################################
-
 # CIRCLE PLOT
 # Setup figure
 fig, ax = plt.subplots(figsize = (12, 12), constrained_layout = True)
@@ -205,5 +132,76 @@ ax.text(
 )
 
 plt.savefig('ejemplos/bosque-chapultepec.png')
+
+###############################################################################
+
+# SQUARE PLOT WITH ROUNDED EDGES
+# Style parameters
+palette = ['#001c5a', '#ffab15']
+background_c = '#e5c6f5'
+dilate = 100
+
+# Setup figure
+fig, ax = plt.subplots(figsize = (10, 10), constrained_layout = True)
+
+# Plot
+layers = plot(
+    (19.449885835076476, -99.15688024541792), radius = 500,
+    ax = ax,
+    layers = {
+        'perimeter': {'circle': False, 'dilate': dilate},
+        'streets': {
+            'width': {
+                'primary': 5,
+                'secondary': 4,
+                'tertiary': 3,
+                'residential': 2,
+                'footway': 1,
+            },
+            'circle': False,
+            'dilate': dilate
+        },
+        'building': {
+            'tags': {'building': True},
+            'union': False,
+            'circle': False,
+            'dilate': dilate
+        },
+        'green': {
+            'tags': {
+                'landuse': ['grass', 'village_green'],
+                'leisure': 'park'
+            },
+            'circle': False,
+            'dilate': dilate
+        },
+    },
+    drawing_kwargs = {
+        'background': {'fc': '#fdf9a0', 'ec': '#ffedac', 'hatch': 'ooo...', 'zorder': -1},
+        'perimeter': {'fill': False, 'lw': 0, 'zorder': 0},
+        'green': {'fc': '#8BB174', 'ec': '#2F3737', 'hatch_c': '#A7C497', 'hatch': 'ooo...', 'lw': 1, 'zorder': 1},
+        'water': {'fc': '#a8e1e6', 'ec': '#2F3737', 'hatch_c': '#9bc3d4', 'hatch': 'ooo...', 'lw': 1, 'zorder': 3},
+        'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 4},
+        'building': {'palette': palette, 'ec': '#005858', 'lw': .5, 'zorder': 5},
+    },
+    osm_credit = {'x': .02, 'y': .01, 'color': '#2F3737'}
+)
+
+# Set bounds
+xmin, ymin, xmax, ymax = layers['perimeter'].bounds
+dx, dy = xmax-xmin, ymax-ymin
+ax.set_xlim(xmin-.06*dx, xmax+.06*dx)
+ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
+
+# Draw right text
+ax.text(
+    xmin+1.02*dx, ymin+.26*dy,
+    'Kiosco Morisco, CDMX, México',
+    color = '#2F3737',
+    rotation = 270,
+    fontproperties = fm.FontProperties(fname = 'assets/fonts/HashedBrowns-WyJgn.ttf', size = 40)
+)
+
+plt.savefig('ejemplos/kiosco-morisco.png')
 
 ###############################################################################
