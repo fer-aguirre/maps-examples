00d73ac0554c052b5cb6787ca7d33861b337ff49
FXR <kay_op@protonmail.com>
2021-09-14 21:03:39 -0500

    Update mapaFolium.py

--- mapaFolium.py
+++ mapaFolium.py
@@ -1,29 +1,47 @@
 # import sys
 # print(sys.executable)
 # /usr/bin/python3 -m pip install folium
-
 import folium
 from folium.features import CustomIcon
+import io
+from PIL import Image
 
-m = folium.Map(location=[19.43274052957381, -99.133221555212], zoom_start=12, tiles="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", attr= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>')
+m = folium.Map(location=[19.43274052957381, -99.133221555212], zoom_start=16, tiles="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", attr= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>')
 
-url = "http://leafletjs.com/examples/custom-icons/{}".format
-icon_image = url("leaf-red.png")
-shadow_image = url("leaf-shadow.png")
-
-icon = CustomIcon(
+icon_image = "icons/park.png"
+icon = folium.features.CustomIcon(
     icon_image,
-    icon_size=(38, 95),
-    icon_anchor=(22, 94),
-    shadow_image=shadow_image,
-    shadow_size=(50, 64),
-    shadow_anchor=(4, 62),
-    popup_anchor=(-3, -76)
-    )
+    icon_size=(50, 40),
+    icon_anchor=(20, 10),
+    popup_anchor=(-3, -76),
+)
+
+tooltip = "¡Haz click!"
+
+folium.Marker([19.435011442107704, -99.13265026035839], icon=icon, popup="<b>Parque República de Guatemala</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.42982058040328, -99.13279050032122], icon=icon, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.43640005202161, -99.14397464702137], icon=icon, popup="<b>Alameda Central</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.433655882695284, -99.14372922687433], icon=icon, popup="<b>Parque Santos Degollado</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.426977108120735, -99.14572764812497], icon=icon, popup="<b>Parque Pujibet</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.42509245375351, -99.14246706617165], icon=icon, popup="<b>Parque Fray servando Teresa de Mier</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.42429890853207, -99.14001286470138], icon=icon, popup="<b>Parque Gilberto Owen</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.422711806416164, -99.12774185713113], icon=icon, popup="<b>Parque El Indio</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.43504749580566, -99.11170875643528], icon=icon, popup="<b>Parque San Antonio Tomatlan</b>", tooltip=tooltip).add_to(m)
+
+folium.Marker([19.436423469877685, -99.10921966656204], icon=icon, popup="<b>Parque Héroe de Nacozari</b>", tooltip=tooltip).add_to(m)
 
 
-tooltip = "Click me!"
-
-folium.Marker([19.435814905084374, -99.13233435292825], popup="<i>Librería Nely</i>", tooltip=tooltip).add_to(m)
-folium.Marker([19.4358958446759, -99.13158333443202], popup="<b>Librería Porrúa</b>", tooltip=tooltip).add_to(m)
-m.save("output/index.html")
+# Save as 'html'
+m.save("ejemplos/index.html")
+# Save as 'png'
+img_data = m._to_png(5)
+img = Image.open(io.BytesIO(img_data))
+img.save('ejemplos/cdmx-folium.png')
