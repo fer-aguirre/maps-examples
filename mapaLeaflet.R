893a47d7039688c3d0bfe8c4dfb12af1bff0e24f
FXR <kay_op@protonmail.com>
2021-09-15 13:39:43 -0500

    Update mapaLeaflet.R

--- mapaLeaflet.R
+++ mapaLeaflet.R
@@ -12,7 +12,7 @@ m <- leaflet() %>%
   setView( lat=19.432750647216512, lng=-99.13317863986936, zoom=14) %>%
   # Add layers
   addProviderTiles("Stamen.Toner") %>%
-  # Markers
+  # Add markers
   addMarkers(m, lat=19.435011442107704, lng=-99.13265026035839, popup="<b>Parque República de Guatemala</b>", icon=icon_park) %>%
   addMarkers(m, lat=19.42982058040328, lng=-99.13279050032122, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>", icon=icon_park) %>%
   addMarkers(m, lat=19.43640005202161, lng=-99.14397464702137, popup="<b>Alameda Central</b>", icon=icon_park) %>%
