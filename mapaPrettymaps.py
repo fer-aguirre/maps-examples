ad2a124cf1433e7783813435ab1c0e70cfcf54ae
FXR <kay_op@protonmail.com>
2021-09-15 01:32:50 -0500

    Add examples

--- mapaPrettymaps.py
+++ mapaPrettymaps.py
@@ -2,7 +2,6 @@ from prettymaps import *
 import matplotlib.font_manager as fm
 from matplotlib import pyplot as plt
 
-
 # SQUARE PLOT WITH ROUNDED EDGES
 # Style parameters
 palette = ['#001c5a', '#ffab15']
@@ -14,7 +13,7 @@ fig, ax = plt.subplots(figsize = (10, 10), constrained_layout = True)
 
 # Plot
 layers = plot(
-    (19.43274052957381, -99.133221555212), radius = 500,
+    (19.449885835076476, -99.15688024541792), radius = 500,
     ax = ax,
     layers = {
         'perimeter': {'circle': False, 'dilate': dilate},
@@ -63,12 +62,15 @@ ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
 
 # Draw top text
 ax.text(
-    xmax-.30*dx, ymax+.01*dy,
-    'CDMX, México',
+    #xmin-.05*dx, ymin-.06*dy,
+    xmin+1.02*dx, ymin+.26*dy,
+    'Kiosco Morisco, CDMX, México',
     color = '#2F3737',
-    fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40))
+    rotation = 270,
+    fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40)
+)
 
-plt.savefig('ejemplos/cdmx-square.png')
+plt.savefig('ejemplos/kiosco-morisco.png')
 
 
 ###############################################################################
@@ -128,13 +130,80 @@ ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
 
 # Draw top text
 ax.text(
-    xmax-.25*dx, ymax-.02*dy,
-    'CDMX, México',
+    xmax-.25*dx, ymax-.05*dy,
+    'Zócalo,\nCDMX, México',
     color = '#2F3737',
-    fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40),
+    fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40)
 )
 
 
-plt.savefig('ejemplos/cdmx-circle.png')
+plt.savefig('ejemplos/zocalo.png')
+
+###############################################################################
+
+# BOUNDARY DEFINED BY GEOGRAPHY
+def postprocessing(layers):
+    layers['perimeter'] = layers['perimeter'].buffer(10)
+    return layers
+
+fig, ax = plt.subplots(figsize = (15, 12), constrained_layout = True)
+
+layers = plot(
+    'Bosque de Chapultepec, Miguel Hidalgo, Ciudad de México',
+
+    ax = ax,
+
+    postprocessing = postprocessing,
+
+    layers = {
+            'perimeter': {},
+            'streets': {
+                'width': {
+                    #'motorway': 8,
+                    'trunk': 6,
+                    'primary': 6,
+                    'secondary': 5,
+                    'tertiary': 4,
+                    'residential': 3,
+                    #'living_street': 3,
+                    'pedestrian': 1.5,
+                    'footway': 1.5,
+                    #'track': 1,
+                    #'bridleway': 1
+                }
+            },
+            'building': {'tags': {'building': True}, 'union': False},
+            'water': {'tags': {'natural': ['water', 'bay']}},
+            'green': {'tags': {'landuse': 'grass', 'natural': ['island', 'wood'], 'leisure': 'park'}},
+        },
+        drawing_kwargs = {
+            'background': {'fc': '#F9BACA', 'ec': '#F4A5B9', 'hatch': 'ooo...', 'zorder': -1},
+            'perimeter': {'fc': '#F7F3F5', 'ec': '#2F3737', 'lw': 3, 'hatch': 'ooo...', 'hatch_c': '#EFE7EB',  'zorder': 0},
+            'green': {'fc': '#AABD8C', 'ec': '#2F3737', 'hatch_c': '#b3cfa5', 'hatch': 'ooo...', 'lw': 0, 'zorder': 1},
+            'water': {'fc': '#a1e3ff', 'hatch': 'ooo...', 'hatch_c': '#80bed9', 'lw': 0, 'zorder': 2},
+            'streets': {'fc': '#3b4545', 'lw': 0, 'zorder': 3},
+            'building': {'palette': ['#433633', '#FF5E5B'], 'ec': '#2F3737', 'hatch': 'ooo...', 'hatch_c': '#b3504f', 'lw': 0, 'zorder': 4},
+        },
+
+        osm_credit = {'color': '#3b4545'}
+)
+
+# Set bounds
+xmin, ymin, xmax, ymax = layers['perimeter'].bounds
+dx, dy = xmax-xmin, ymax-ymin
+ax.set_xlim(xmin-.06*dx, xmax+.06*dx)
+ax.set_ylim(ymin-.06*dy, ymax+.06*dy)
+
+
+# Draw left text
+ax.text(
+    xmin-.06*dx, ymin+.5*dy,
+    'Bosque de Chapultepec,\nCDMX, México',
+    color = '#2F3737',
+    rotation = 90,
+    fontproperties = fm.FontProperties(fname = 'fonts/HashedBrowns-WyJgn.ttf', size = 40)
+)
+
+plt.savefig('ejemplos/bosque-chapultepec.png')
 
 ###############################################################################
