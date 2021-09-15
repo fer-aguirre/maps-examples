bff3665964c57a034f0bf379930db060ea43e0be
FXR <kay_op@protonmail.com>
2021-09-15 13:39:22 -0500

    Update mapaFolium.py

--- mapaFolium.py
+++ mapaFolium.py
@@ -17,19 +17,14 @@ icon = CustomIcon(
     icon_anchor=(20, 10),
     popup_anchor=(-3, -76),
 )
-
+# Custom tooltip message
 tooltip = "¡Haz click!"
-
+# Add markers
 folium.Marker([19.435011442107704, -99.13265026035839], icon=icon, popup="<b>Parque República de Guatemala</b>", tooltip=tooltip).add_to(m)
-
-folium.Marker([19.42982058040328, -99.13279050032122], icon=icon, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>", tooltip=tooltip).add_to(m)
-
+folium.Marker([19.42982058040328, -99.13279050032122], icon=icon, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>",          tooltip=tooltip).add_to(m)
 folium.Marker([19.43640005202161, -99.14397464702137], icon=icon, popup="<b>Alameda Central</b>", tooltip=tooltip).add_to(m)
-
 folium.Marker([19.433655882695284, -99.14372922687433], icon=icon, popup="<b>Parque Santos Degollado</b>", tooltip=tooltip).add_to(m)
-
 folium.Marker([19.426977108120735, -99.14572764812497], icon=icon, popup="<b>Parque Pujibet</b>", tooltip=tooltip).add_to(m)
-
 
 # Save as 'html'
 m.save("ejemplos/folium-python.html")
