deaec31b6f00424506496a49f151ca0a7f2de052
FXR <kay_op@protonmail.com>
2021-09-15 12:58:11 -0500

    Update mapaLeaflet.R

--- mapaLeaflet.R
+++ mapaLeaflet.R
@@ -14,10 +14,10 @@ m <- leaflet() %>%
   addProviderTiles("Stamen.Toner") %>%
   # Markers
   addMarkers(m, lat=19.435011442107704, lng=-99.13265026035839, popup="<b>Parque República de Guatemala</b>", icon=icon_park) %>%
-  addMarkers(m, lat=19.42982058040328, lng=-99.13279050032122, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>", icon=icon) %>%
-  addMarkers(m, lat=19.43640005202161, lng=-99.14397464702137, popup="<b>Alameda Central</b>", icon=icon) %>%
-  addMarkers(m, lat=19.433655882695284, lng=-99.14372922687433, popup="<b>Parque Santos Degollado</b>", icon=icon) %>%
-  addMarkers(m, lat=19.426977108120735, lng=-99.14572764812497, popup="<b>Parque Pujibet</b>", icon=icon)
+  addMarkers(m, lat=19.42982058040328, lng=-99.13279050032122, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>", icon=icon_park) %>%
+  addMarkers(m, lat=19.43640005202161, lng=-99.14397464702137, popup="<b>Alameda Central</b>", icon=icon_park) %>%
+  addMarkers(m, lat=19.433655882695284, lng=-99.14372922687433, popup="<b>Parque Santos Degollado</b>", icon=icon_park) %>%
+  addMarkers(m, lat=19.426977108120735, lng=-99.14572764812497, popup="<b>Parque Pujibet</b>", icon=icon_park)
 m
 
 
