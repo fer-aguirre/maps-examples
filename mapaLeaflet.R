01d0f316d698fdf819311a74c11f479352e51bb7
FXR <kay_op@protonmail.com>
2021-09-15 12:50:24 -0500

    Update mapaLeaflet.R

--- mapaLeaflet.R
+++ mapaLeaflet.R
@@ -1,30 +1,24 @@
 library(leaflet)
 library(here)
-library(shiny)
 
-
-mapa <- read.csv("huertos.csv")
-options("scipen"=100, "digits"=15)
-
-popup = paste0( paste("<b>", mapa$nombre, "</b><hr>", sep = "")
-                , "<br><b>Tipo de Huerto:</b> "
-                , mapa$tipo
-                , "<br><br><b>Descripción:</b> "
-                , mapa$descripcion
-                , paste('<br><br><b><a href="', mapa$redes_sociales, '" target=“_blank”>Redes Sociales</a>', sep = "")
-)
-
-icono_semilla <- makeIcon(
-  iconUrl = "plant.png",
-  iconWidth = 40, iconHeight = 50,
+# Custom icon
+icon_park <- makeIcon(
+  iconUrl = "assets/icons/park.png",
+  iconWidth = 30, iconHeight = 40,
   iconAnchorX = 0, iconAnchorY = 0
 )
 
+m <- leaflet() %>% 
+  setView( lat=19.432750647216512, lng=-99.13317863986936, zoom=14) %>%
+  # Add layers
+  addProviderTiles("Stamen.Toner") %>%
+  # Markers
+  addMarkers(m, lat=19.435011442107704, lng=-99.13265026035839, popup="<b>Parque República de Guatemala</b>", icon=icon_park) %>%
+  addMarkers(m, lat=19.42982058040328, lng=-99.13279050032122, popup="<b>Parque Francisco Primo De Verdad Y Ramos</b>", icon=icon) %>%
+  addMarkers(m, lat=19.43640005202161, lng=-99.14397464702137, popup="<b>Alameda Central</b>", icon=icon) %>%
+  addMarkers(m, lat=19.433655882695284, lng=-99.14372922687433, popup="<b>Parque Santos Degollado</b>", icon=icon) %>%
+  addMarkers(m, lat=19.426977108120735, lng=-99.14572764812497, popup="<b>Parque Pujibet</b>", icon=icon)
+m
 
-leaflet() %>% 
-  setView( lat=20, lng=-100, zoom=5) %>%
-  addProviderTiles("Stamen.Watercolor") %>%
-  addProviderTiles("Stamen.TonerLabels") %>%
-  addMarkers(data = mapa, ~longitud, ~latitud, icon = icono_semilla,
-             popup =  popup)
 
+  
